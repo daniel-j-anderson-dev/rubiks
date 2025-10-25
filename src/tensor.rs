d0ff34f2e989a95dd0460c6b637415e41aedf67f
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-10-25 16:25:06 -0600

    added doc comments

--- src/tensor.rs
+++ src/tensor.rs
@@ -1,9 +1,17 @@
 /// A 3-dimensional array with it's shape listed in descending order form `OUTER_MOST` to `INNER_MOST`  
-pub type Tensor3<T, const OUTER_DIM: usize, const MIDDLE_DIM: usize, const INNER_DIM: usize> =
-    [[[T; INNER_DIM]; MIDDLE_DIM]; OUTER_DIM];
+pub type Tensor3<T, const DIM0: usize, const DIM1: usize, const DIM2: usize> =
+    [[[T; DIM2]; DIM1]; DIM0];
 
-pub fn tensor3_from_fn<T, const H: usize, const W: usize, const D: usize>(
+/// constructs a [Tensor3] from a function `f` that is given an index for each element.
+/// # Parameters
+/// - `f`
+///   - `parameter0`: index into `dimension0` (increases the slowest)
+///   - `parameter1`: index into `dimension1`
+///   - `parameter2`: index into `dimension1` (increases the fastest)
+///   - `returns`: the value of the output [Tensor3] at index `parameter0, parameter1, parameter2`
+///
+pub fn tensor3_from_fn<T, const DIM0: usize, const DIM1: usize, const DIM2: usize>(
     mut f: impl FnMut(usize, usize, usize) -> T,
-) -> Tensor3<T, H, W, D> {
+) -> Tensor3<T, DIM0, DIM1, DIM2> {
     core::array::from_fn(|i| core::array::from_fn(|j| core::array::from_fn(|k| f(i, j, k))))
 }
