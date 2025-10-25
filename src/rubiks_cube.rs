48a64bdecb776addfd81f221038a3d32b3d349f4
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-10-25 17:12:16 -0600

    separated impl blocks

--- src/rubiks_cube.rs
+++ src/rubiks_cube.rs
@@ -1,27 +1,41 @@
-use crate::*;
+pub mod color;
+pub mod face;
+pub mod rotation;
 
+pub use self::{color::*, face::*, rotation::*};
+
+use crate::tensor::*;
+
+pub const RANK: usize = 3;
 pub const FACES_COUNT: usize = 6;
 pub const ROW_COUNT: usize = 3;
 pub const COLUMN_COUNT: usize = 3;
-pub const SHAPE: [usize; 3] = [COLUMN_COUNT, ROW_COUNT, COLUMN_COUNT];
+pub const SHAPE: [usize; RANK] = [COLUMN_COUNT, ROW_COUNT, COLUMN_COUNT];
 
 #[derive(Debug, Clone, Copy)]
 pub struct RubiksCube {
-    /// | Face | Color  | Reason |
-    /// | ---- | ------ | ------ |
-    /// | 0    | White  | Up     |
-    /// | 1    | Blue   | Left   |
-    /// | 2    | Orange | Front  |
-    /// | 3    | Red    | Back   |
-    /// | 4    | Green  | Right  |
-    /// | 5    | Yellow | Down   |
     faces: Tensor3<Color, { FACES_COUNT }, { ROW_COUNT }, { COLUMN_COUNT }>,
 }
+/// Constructors
 impl RubiksCube {
     pub fn new() -> Self {
-        let colors = [White, Orange, Blue, Red, Green, Yellow];
         Self {
-            faces: tensor3_from_fn(|face_index, _, _| colors[face_index]),
+            faces: tensor3_from_fn(|face_index, _, _| Face::COLORS[face_index]),
         }
+    }
+}
+/// Accessors
+impl RubiksCube {
+    pub fn face(&self, face: Face) -> &[[Color; COLUMN_COUNT]; ROW_COUNT] {
+        &self.faces[face as usize]
+    }
+    pub fn face_mut(&mut self, face: Face) -> &mut [[Color; COLUMN_COUNT]; ROW_COUNT] {
+        &mut self.faces[face as usize]
+    }
+}
+/// Mutators
+impl RubiksCube {
+    pub fn rotate(&mut self, rotation: Rotation) {
+        todo!()
     }
 }
