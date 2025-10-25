489e5c86bc49741dcb9dab344c2cc545420fdd1b
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-10-25 16:09:28 -0600

    made const params more explicit

--- src/tensor.rs
+++ src/tensor.rs
@@ -1,4 +1,5 @@
-pub type Tensor3<T, const H: usize, const W: usize, const D: usize> = [[[T; D]; W]; H];
+pub type Tensor3<T, const OUTER_MOST: usize, const MIDDLE: usize, const INNER_MOST: usize> =
+    [[[T; INNER_MOST]; MIDDLE]; OUTER_MOST];
 
 pub fn tensor3_from_fn<T, const H: usize, const W: usize, const D: usize>(
     mut f: impl FnMut(usize, usize, usize) -> T,
