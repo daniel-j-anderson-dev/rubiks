2a1b16ae153cb08f5ffaaee93255b0ee6ee91437
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-10-25 17:11:36 -0600

    changed module tree structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-pub mod color;
-pub mod rotation;
 pub mod rubiks_cube;
 pub mod tensor;
 
-pub use crate::{color::*, rotation::*, rubiks_cube::*, tensor::*};
+pub use crate::{rubiks_cube::*, tensor::*};
