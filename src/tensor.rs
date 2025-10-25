1c2ca9568f7fee1f1a04c2885c926d578fe983bb
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-10-25 16:11:42 -0600

    changed  const param names

--- src/tensor.rs
+++ src/tensor.rs
@@ -1,5 +1,6 @@
-pub type Tensor3<T, const OUTER_MOST: usize, const MIDDLE: usize, const INNER_MOST: usize> =
-    [[[T; INNER_MOST]; MIDDLE]; OUTER_MOST];
+/// A 3-dimensional array with it's shape listed in descending order form `OUTER_MOST` to `INNER_MOST`  
+pub type Tensor3<T, const OUTER_DIM: usize, const MIDDLE_DIM: usize, const INNER_DIM: usize> =
+    [[[T; INNER_DIM]; MIDDLE_DIM]; OUTER_DIM];
 
 pub fn tensor3_from_fn<T, const H: usize, const W: usize, const D: usize>(
     mut f: impl FnMut(usize, usize, usize) -> T,
